7fd29a8b8d92768bef94015f9561052dcee10844
Robosword <32asingh@stratfordschools.net>
2026-05-21 16:09:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 
+from flask import Flask, render_template, jsonify, session
+app = Flask(__name__)
+
+@app.route("/")
+def terminal():
+  return render_template("terminal.html")
+
+@app.route("/sendCommand")
+def sendGetCommand():
+  pass
+
+if __name__ == "__main__":
+  app.run(debug=True)

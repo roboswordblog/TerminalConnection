81bda4ac97b5b62029edbcef8c6a1d36bbdefd1d
Robosword <32asingh@stratfordschools.net>
2026-05-22 11:29:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 from flask import Flask, render_template, jsonify, session
 app = Flask(__name__)
 app.secret_key = '8767'
-
+# set type to pi5 for the pi5, set type to bob for the normal computer
+session[]
 @app.route("/")
 def terminal():
   return render_template("terminal.html")

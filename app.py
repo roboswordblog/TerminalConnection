e4f58249c183c866e3d1ec7c08c0e045937d4c4b
Robosword <32asingh@stratfordschools.net>
2026-05-22 18:16:02 -0700

    Reformatting.

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import subprocess
 from flask import Flask, render_template, jsonify, session, request
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def terminal():
-  return render_template("terminal.html")
+    return render_template("terminal.html")
+
 
 @app.route("/sendCommand")
 def sendGetCommand():
-  result = subprocess.run(request.get_json("message"), capture_output=True, text=True)
+    result = subprocess.run(request.get_json("message"), capture_output=True, text=True)
 
-  return jsonify({"output": result.stdout})
+    return jsonify({"output": result.stdout})
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
